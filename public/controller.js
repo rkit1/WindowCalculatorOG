1976db28bc350f9c743c8d149eec9fa4d840b97e
Victor Gorokhov <me@rkit.pp.ru>
2013-06-03 23:29:30 +0400

    Пам-пам-пам

--- public/controller.js
+++ public/controller.js
@@ -1,21 +1,56 @@
-angular.module('Calc', []);
+angular.module('Calc', ['ngCookies']);
 DefaultWindow = function(w){
-    this.name = 'Новое окно';
-    this.panesCount = 1;
-    this.panes = [{type:'solid'},{type:'solid'},{type:'solid'}];
+    //this.name = 'Новое окно';
+    this.type = '1p';
+    this.panes = [{type:'solid', width:50},{type:'solid', width:50},{type:'solid', width:50}];
+    this.width = 120;
+    this.height = 100;
+    this.steklopaket = 1;
+    this.laminate = false;
+    this.profile = 1;
     return this;
-}
-DefaultWindow.prototype.setPanesCount = function (c){
-    this.panesCount = c;
-}
-function CalcController($scope, $http) {
-    $scope.currentWindow = new DefaultWindow();
-    $scope.windows = [$scope.currentWindow];
+};
+DefaultWindow.prototype.getPanesCount = function(){
+    switch (this.type)
+    {
+        case '1p': return 1;
+        case '2p': return 2;
+        case '3p': return 3;
+    }
+    return 1;
+};
+DefaultWindow.prototype.getActivePanes = function(){
+    if (this.type == 'door') return [];
+    return this.panes.slice(0, this.getPanesCount());
+};
+function CalcController($scope, $cookies) {
     $scope.newWindow = function(){
         $scope.windows[$scope.windows.length] =
             $scope.currentWindow = new DefaultWindow($scope.currentWindow);
-    }
+    };
     $scope.selectWindow = function(w){
         $scope.currentWindow = $scope.windows[w];
+    };
+    //FIXME
+    $scope.removeWindow = function(w) {
+        delete $scope.windows[$scope.windows.indexOf(w)];
+        if ($scope.windows.length < 1) {
+            $scope.currentWindow = new DefaultWindow();
+            $scope.windows = [$scope.currentWindow];
+        }
+    };
+    $scope.reset = function(){
+        $scope.currentWindow = new DefaultWindow();
+        $scope.windows = [$scope.currentWindow];
     }
+    if ($cookies.windows != null){
+        $scope.windows = $cookies.windows;
+        $scope.currentWindow = $scope.windows[0];
+    }
+    else $scope.reset();
+    $scope.$on('$locationChangeStart', function (event, next, current) {
+        alert('asd');
+        $cookies.windows = $scope.windows;
+    });
+
 }
