b86211b826459be7525faaa652a1437729812603
Victor Gorokhov <me@rkit.pp.ru>
2013-06-05 18:38:46 +0400

    Вычистил к хуям формулы, т.к. они кривые оказались

--- public/controller.js
+++ public/controller.js
@@ -9,27 +9,7 @@ Window = function(w){
     this.steklopaket = 1;
     this.laminate = 0;
     this.profile = 1;
-    this.framugi = 0; // C16. Не используется
-    this.heightOfTPart = 0; // C13. Не используется
-    this.shtulpLenght = 0; // C11. Не используется
-    this.raise = 20; // C30 FIXME
-    this.sandwichHeight = 0; // C18. Не используется
-    this.sandvichColorCoefficient = 0; // C19. Не используется
-    this.numOfFramuges = 0; // С16. Не используется
     return this;
-};
-Window.prototype.getPanesOfType = function(type) {
-    var panes = this.getActivePanes();
-    var out = [];
-    for (var i in panes) if (panes[i].type = type) out[out.length] = panes[i];
-    return out;
-};
-Window.prototype.getSizeError = function() {
-    var aPanes = this.getActivePanes();
-    var w = this.width;
-    for (var p in aPanes)
-        w -= aPanes[p].width;
-    return w;
 };
 Window.prototype.getPanesCount = function(){
     switch (this.type)
@@ -45,67 +25,6 @@ Window.prototype.getActivePanes = function(){
     return this.panes.slice(0, this.getPanesCount());
 };
 
-Window.prototype.getLaminateK = function() {
-    // 45 из B21, (1+C21/100)/1000 из C23. Вынесено в отдельную функцию из-за частоты употребления
-    return (1 + (this.laminate * 45 / 100))/1000;
-};
-Window.prototype.getFramePrice = function() {
-    // ((C8+C9)*2*9*(1+C21/100)/1000 из C23
-    return (this.width + this.height) * 18 * this.getLaminateK()
-};
-Window.prototype.getImpostsLength = function() {
-    return this.height * (this.getPanesCount() - 1)
-};
-Window.prototype.getImpostsPrice = function() {
-    // C10*14*(1+C21/100)/1000 из C23
-    return this.getImpostsLength() * 14 * this.getLaminateK();
-};
-Window.prototype.getShtulpPrice = function() {
-    // C11*18*(1+C21/100)/1000 из C23
-    return this.shtulpLenght * 18 * this.getLaminateK();
-};
-Window.prototype.getStvorkiAndFramugiWidth = function() {
-    // C12
-    var w = 0;
-    var panes = this.getActivePanes();
-    for (var i in panes)
-        if (panes[i].type == 'r' || panes[i].type == 'ro') w += panes[i].width;
-    return w;
-};
-Window.prototype.getStvorkiAndFramugiPrice = function(){
-    // (C12*2+((C14+C15)*(C9-C13)*2+C16*C13*2)*9*(1+C21/100)/1000+C14*70+C15*85+C16*45) из C23
-    return ( this.getStvorkiAndFramugiWidth() * 2
-           + (this.getPanesOfType('rot').length + this.getPanesOfType('rotdrop').length)/(this.height - this.heightOfTPart) * 2
-           + this.numOfFramuges * this.heightOfTPart * 2
-           )
-         /* * 9
-         * ( this.getLaminateK()
-           + this.getPanesOfType('rot') * 70
-           + this.getPanesOfType('rotdrop') * 85
-           + this.framugi * 45 );
-           */
-};
-Window.prototype.getAreaRaise = function(){
-    // (C8*C9)*C17/1000000 из C23
-    return (this.width + this.height) / 1000000 * this.raise;
-};
-Window.prototype.getSandwichPrice = function(){
-    // C8*C18*C19 из C23
-    return this.width * this.sandwichHeight * this.sandvichColorCoefficient;
-};
-Window.prototype.getGlassPrice = function() {
-    // 47*(C8*C9)/1000000 из C23
-    return 47 * (this.width * this.height) / 1000000;
-};
-Window.prototype.getTotalSelfPrice = function(){
-    // C23
-    return this.getFramePrice() + this.getGlassPrice() + this.getImpostsPrice() + this.getShtulpPrice()
-           + this.getStvorkiAndFramugiPrice() + this.getAreaRaise() + this.getSandwichPrice();
-};
-Window.prototype.getTotalPrice = function(){
-    // C22
-    return Math.ceil(this.getTotalSelfPrice() * 1.3);
-};
 
 function CalcController($scope, $cookies) {
     $scope.newWindow = function(){
