120b6474993a52078c5f64a4ef56091ebbb197e5
Victor Gorokhov <me@rkit.pp.ru>
2013-08-25 23:25:46 +0400

    Готова в закоментированном виде

--- public/orders.php
+++ public/orders.php
@@ -8,7 +8,8 @@ function sendMessage($orderId){
     $mail = new PHPMailer;
     $to = 'me@rkit.pp.ru';
     $from = 'me@rkit.pp.ru';
-    $calcRoot = '';
+    $dn = dirname($_SERVER['REQUEST_URI']);
+    $calcRoot = "http://{$_SERVER['HTTP_HOST']}/{$dn}/";
 
     $mail->From = $from;
     $mail->FromName = 'Mailer';
@@ -17,9 +18,9 @@ function sendMessage($orderId){
     $mail->WordWrap = 50;
     $mail->IsHTML(true);
 
-    $mail->Subject = 'Новый заказ';
-    $mail->Body    = "Принят заказ №<a href=\"\">{$orderId}</a>";
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Subject = 'Новый заказ в калькуляторе окон';
+    $mail->Body    = "Принят заказ №<a href=\"{$calcRoot}/calculator.htm#/order/{$orderId}\">{$orderId}</a>";
+    $mail->AltBody = "Принят заказ №{$orderId} {$calcRoot}/calculator.htm#/order/{$orderId}";
 
     return $mail->Send();
 };
@@ -45,5 +46,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $st = $db->prepare("INSERT INTO orders (time, value) VALUES (NOW(), ?);");
     $st->execute(array($body));
     $result = $db->query("SELECT LAST_INSERT_ID() as id;")->fetch(PDO::FETCH_ASSOC);
+    //if (!sendMessage($result['orderId'])) e(500);
     echo json_encode($result);
 }
